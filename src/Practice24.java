f44b2eff9fd45949bf22b957c24b313c1b01082a
foagura <foagura_0416@yahoo.co.jp>
2024-09-28 21:49:36 +0900

    first commit

---
+++ src/Practice24.java
@@ -0,0 +1,27 @@
+public class Practice24 {
+    int instanceVal;
+    static int staticVal;
+
+    void instanceMethodA() {    // インスタンスメンバ同士で呼び出せる
+        System.out.println("インスタンス変数の値：" + instanceVal);
+    }
+    void instanceMethodB() {    // インスタンスメンバからstaticメンバを呼び出せる
+        System.out.println("static変数の値：" + staticVal);
+    }
+
+    static void staticMethodA() {    // staticメンバ同士で呼び出せる
+        System.out.println("static変数の値：" + staticVal);
+    }
+    static void staticMethodB(Practice24 obj) {    // インスタンス化すれば、staticメンバからでもインスタンスメンバを呼び出せる
+        System.out.println("インスタンス変数の値：" + obj.instanceVal);
+    }
+
+    public static void main(String[] args){
+        Practice24 obj = new Practice24();
+
+        obj.instanceMethodA();
+        obj.instanceMethodB();
+        staticMethodA();
+        staticMethodB(obj);
+    }
+}
